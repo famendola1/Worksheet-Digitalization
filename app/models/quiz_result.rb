08eca9e9a950cb07bf17fb7c46f93f01a096b6a4
Fabio <fabio.amendola20@gmail.com>
2017-10-07 12:01:36 -0700

    Added quiz results to db

--- app/models/quiz_result.rb
+++ app/models/quiz_result.rb
@@ -1,2 +1,3 @@
 class QuizResult < ApplicationRecord
+  belongs_to :student, :foreign_key => :student_id, class_name: 'Student'
 end
--- db/migrate/20171006230308_create_quiz_results.rb
+++ db/migrate/20171006230308_create_quiz_results.rb
@@ -1,6 +1,9 @@
 class CreateQuizResults < ActiveRecord::Migration[5.1]
   def change
     create_table :quiz_results do |t|
+      t.integer :result_id
+      t.references :student_id
+      t.string :reflection
 
       t.timestamps
     end
