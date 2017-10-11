db6b9864703c92380f9b8a61b03f17011f95de7d
Fabio Amendola <fabio.amendola20@gmail.com>
2017-10-11 08:03:50 +0000

    Linked quiz results to students

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,16 @@ Rails.application.routes.draw do
   devise_for :admins
   get 'welcome/index'
 
-  resources :students, param: :student_id
+  resources :students, param: :student_id # do 
+    # resources :quiz_results
+  # end
+  
+  scope 'students/:student_id' do
+    resources :quiz_results
+  end
   
   resources :articles
   
-  resources :quiz_results
-
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
