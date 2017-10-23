308276959346fc51cf57a9de1ff6062a15ef7580
Fabio Amendola <fabio.amendola20@gmail.com>
2017-10-23 19:41:00 +0000

    Add views for admin signup and login

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,18 @@
 Rails.application.routes.draw do
   devise_for :admins
+  
+  authenticated :admin do
+    root 'welcome#index'
+  end
+  
   root 'welcome#index'
-  get 'welcome/index'
-
+  
   # Routing for students and quiz result requests
   resources :students do 
     resources :quiz_results
   end
+  
+  resources :admins
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
