6fe9d5117051d30837309b45638d4cb7cdda6daf
Fabio Amendola <fabio.amendola20@gmail.com>
2017-11-02 22:34:52 +0000

    Refactor export and comments

--- app/models/student.rb
+++ app/models/student.rb
@@ -7,21 +7,26 @@ class Student < ApplicationRecord
     # build attributes
     attributes = %w{student_id name category quiz_id quiz_timestamp quiz_category contributor collaborator communicator challenger }
     (1..18).each do |n|
-      attributes << "q#{n}a"
-      attributes << "q#{n}b"
-      attributes << "q#{n}c"
-      attributes << "q#{n}d"
+      attributes << ["q#{n}a", "q#{n}b", "q#{n}c", "q#{n}d"]
     end
     
+    # generate csv file
     CSV.generate(headers: true) do |csv|
+      # add the attributes as headers
       csv << attributes
+      
       all.each do |s|
+        # relevant student attributes
         student = [s.id, s.name, s.category]
         s.quiz_results.each do |q|
+          # relevant quiz_result attributes
           student.concat [q.id, q.created_at, s.get_category(q), q.contributor, q.collaborator, q.communicator, q.challenger]
           q.answers.order("question ASC").each do |a|
+            # relevant answer attributes
             student.concat [a.responseA, a.responseB, a.responseC, a.responseD]
           end
+          
+          # add this student/quiz_result entry as a row in the csv
           csv << student
         end
       end
